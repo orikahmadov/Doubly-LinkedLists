f1438ba129a5a4c8354b004a7aaa55c4905d03ae
orikahmadov <55695784+orikahmadov@users.noreply.github.com>
2020-05-21 19:19:02 +0200

    Update DoublyLinked.py

--- DoublyLinked.py
+++ DoublyLinked.py
@@ -1,31 +1,29 @@
 class Node:
+    # Node class constructor 
     def __init__(self,data):
         self.data =  data
         self.next = None
         self.prev =  None
 
+        
 class DoublyLinkedList:
     def __init__(self):
         self.head = None
 
-
+#Class method used to add new data to the Node 
     def push(self,NewValue):
         NewNode = Node(NewValue)
         NewNode.next = self.head
         if self.head is not None:
             self.head.prev = NewNode
         self.head = NewNode
-
+#Used for Printing the value 
     def Print(self,node):
         while node is not None:
             print(node.data),
             last  = node
             node = node.next
 
-
+# Create new Head and Head will point to the node
 Family =  DoublyLinkedList()
-Family.push("Sadig Ahmadov")
-Family.push("Orkhan Ahmadov")
-Family.push("Sanan Ahmadov")
-
 Family.Print(Family.head)
